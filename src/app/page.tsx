dfb07853945242a0d7d4cba50b948a6099831dc0
FATIH ATTALA SASTYA PUTRA <fatihattala666@gmail.com>
2024-12-17 02:41:15 +0700

    feat : Added Information Card,  Attandance Charts, and Today Subjects Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,35 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Button } from "@/components/ui/button"
-import { Card, CardFooter, CardHeader } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+import SubjectCard from "@/components/subject-card";
+import AttendanceChart from "@/components/attendance-chart";
+
+const cardData = [
+  {
+    title: "Total Subject",
+    description: "Total Subject You Get",
+    total: 20,
+  },
+  {
+    title: "Attendance",
+    description: "Submitted Attendance",
+    total: 20,
+  },
+  {
+    title: "Assignment",
+    description: "Submitted Assignment",
+    total: 20,
+  },
+  {
+    title: "Rank Table",
+    description: "Rank Of All User",
+    total: 20,
+  },
+];
 
 export default function Page() {
   return (
@@ -28,9 +43,7 @@ export default function Page() {
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
@@ -41,27 +54,44 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <Card className="w-full rounded-xl flex flex-col">
+          <Card className="flex w-full flex-col rounded-xl">
             <CardHeader>
-              <h1 className="text-xl font-bold">Halo Alif Mahendra</h1>
+              <h1 className="text-2xl font-bold">Halo Alif Mahendra</h1>
               <p className="-mt-1">hari yang indah untuk mengerjakan tugasmu, hehe</p>
             </CardHeader>
             <CardFooter>
-              <Button>
-                Njay
-              </Button>
+              <Button>Check Schedule</Button>
             </CardFooter>
           </Card>
           <div className="grid auto-rows-min gap-4 md:grid-cols-4">
-            <Card className="flex">
-              <CardHeader>
-                <b>Total Subject</b>
-              </CardHeader>
-            </Card>
+            {cardData.map((data) => (
+              <Card className="flex flex-row justify-between" key={data.title}>
+                <CardHeader>
+                  <h1 className="text-xl font-bold">{data.title}</h1>
+                  <p className="text-sm">{data.description}</p>
+                </CardHeader>
+                <CardHeader>
+                  <h1 className="text-5xl font-bold">{data.total}</h1>
+                </CardHeader>
+              </Card>
+            ))}
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="ml-1">
+            <h1 className="text-xl font-bold">Attendance Persentage</h1>
+            <span className="text-sm">Attendance Percentage Each Month</span>
+          </div>
+          <div>
+            <AttendanceChart />
+          </div>
+          <div className="ml-1">
+            <h1 className="text-xl font-bold">Today Subject</h1>
+            <span className="text-sm">Senin 23 Januari 2024</span>
+          </div>
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <SubjectCard />
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
---
+++ src/components/subject-card.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader, CardContent } from "@/components/ui/card";
+
+const todaySubjectData = [
+  {
+    id: 1,
+    title: "Dasar Dasar Pemprograman",
+    teacher: "Pak SiDragon",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta tempore neque ipsum deleniti nostrum dignissimos ducimus voluptas corporis. Voluptates voluptate quibusdam facilis ad quo, aperiam necessitatibus vitae accusantium rerum.",
+    notifications: 1,
+  },
+  {
+    id: 2,
+    title: "Dasar Dasar Komputasi",
+    teacher: "Pak Jatmiko",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta tempore neque ipsum deleniti nostrum dignissimos ducimus voluptas corporis. Voluptates voluptate quibusdam facilis ad quo, aperiam necessitatibus vitae accusantium rerum.",
+    notifications: 1,
+  },
+  {
+    id: 3,
+    title: "Keterampilan Interpersonal",
+    teacher: "Bu Siti",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla soluta tempore neque ipsum deleniti nostrum dignissimos ducimus voluptas corporis. Voluptates voluptate quibusdam facilis ad quo, aperiam necessitatibus vitae accusantium rerum.",
+    notifications: 1,
+  },
+];
+
+const SubjectCard = () => {
+  return (
+    <>
+      {todaySubjectData.map((data) => (
+        <Card className="flex flex-col" key={data.id}>
+          <CardHeader>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col">
+                <h1 className="text-xl font-bold">{data.title}</h1>
+                <p className="text-sm">{data.teacher}</p>
+              </div>
+              <Button>
+                <Icon icon="tabler:dots" style={{ width: "24px", height: "24px" }} />
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <p>{data.description}</p>
+          </CardContent>
+          <CardFooter>
+            <p className="text-sm font-light">1 Notifications</p>
+          </CardFooter>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default SubjectCard;
