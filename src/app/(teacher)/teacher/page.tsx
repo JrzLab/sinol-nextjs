b78b1861222b8f666008d2cc0ddc370687c2756e
derispewss <derisfirmansyah177@gmail.com>
2025-01-01 20:35:06 +0700

    feat: idk

--- src/app/(teacher)/teacher/page.tsx
+++ src/app/(teacher)/teacher/page.tsx
@@ -1,9 +1,10 @@
-import { ActivityChart } from '@/app/chart/ActivityChart';
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
-import { chartStaticData1 } from '@/lib/staticData';
-import Link from 'next/link';
-import React from 'react'
+import ClassroomSchedule from "@/components/table/classroom-schedule";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import { getGreeting } from "@/lib/functions";
+import { chartStaticData1, subjectStaticData } from "@/lib/staticData";
+import Link from "next/link";
+import React from "react";
 
 const TeacherPage = () => {
   const cardData = [
@@ -31,24 +32,24 @@ const TeacherPage = () => {
   const greeting = getGreeting();
   return (
     <>
-      <div className='grid gap-2 lg:grid-cols-4 md:grid-cols-2 grid-cols-1'>
-        { cardData.map((doc) => (
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
+        {cardData.map((doc) => (
           <Card key={doc.title}>
-            <CardHeader className='flex flex-row items-start gap-4'>
-              <div className='grid'>
-                <Link href={doc.title} className='hover:underline font-bold'>{doc.title}</Link>
+            <CardHeader className="flex flex-row items-start gap-4">
+              <div className="grid">
+                <Link href={doc.title} className="font-bold hover:underline">
+                  {doc.title}
+                </Link>
                 <p>{doc.description}</p>
               </div>
-            </CardHeader>   
+            </CardHeader>
             <CardFooter>
-              <b className=' text-2xl'>{doc.total}</b>
+              <b className="text-2xl">{doc.total}</b>
             </CardFooter>
           </Card>
         ))}
       </div>
-      <div>
-        {/* <ActivityChart/> */}
-      </div>
+      <div>{/* <ActivityChart/> */}</div>
       <Card className="mx-auto w-full">
         <CardHeader className="pb-0">
           <CardTitle className="text-2xl font-bold tracking-tight">Jadwal</CardTitle>
